15b3dcba6fdf43947d09db78232c42890ff9f00d
Shomii <kkmisha@aol.com>
2018-03-09 12:30:59 +0100

    Space Shooter

---
+++ Assets/Scripts/KontrolerIgraca.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class GranicaIgranja {
+
+	public float xMin, xMax, zMin, zMax;
+}
+
+public class KontrolerIgraca : MonoBehaviour {
+
+	public float speed;
+	public float tilt;
+	public GranicaIgranja granicaIgranja;
+
+	public GameObject pucanj;
+	//public Transform kreiranjeMetka;
+	public Transform[] kreiranjeMetaka;
+	public float brzinaPucanja;
+
+	private Rigidbody rb;
+	private AudioSource zvuk;
+	private float sledeciPucanj;
+
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+		zvuk = GetComponent<AudioSource> ();
+	}
+
+	void Update(){
+		if (Input.GetButton ("Fire1") && Time.time > sledeciPucanj) {
+			sledeciPucanj = Time.time + brzinaPucanja;
+			foreach (var kreiranjeMetka in kreiranjeMetaka){
+			Instantiate (pucanj, kreiranjeMetka.position, kreiranjeMetka.rotation); 
+			}
+				zvuk.Play ();
+
+		}
+	}
+
+
+	void FixedUpdate (){
+	
+		float pomerajSeHorizontalno = Input.GetAxis ("Horizontal");
+		float pomerajSeVertikalno = Input.GetAxis ("Vertical");
+
+		Vector3 kretanjeBroda = new Vector3 (pomerajSeHorizontalno,0.0f,pomerajSeVertikalno);
+		rb.velocity = kretanjeBroda * speed;
+
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, granicaIgranja.xMin, granicaIgranja.xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, granicaIgranja.zMin, granicaIgranja.zMax)
+		);
+
+		rb.rotation = Quaternion.Euler (0.0f, 0.0f, rb.velocity.x * -tilt);
+	}
+
+}
+
---
+++ Assets/Scripts/KontrolerIgre.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+//[RequireComponent(typeof(GUIText))]
+public class KontrolerIgre : MonoBehaviour {
+
+	public GameObject[] opasnostiZaIgraca;
+	public Vector3 parametriAsteroida;
+	public int brojNaleta;
+	public float pauzaUKreiranjuNaleta;
+	public float pocetakPauze;
+	public float pauzaUNaletima;
+//	public GUIText rezultatTekst;
+	public Text rezultatTekst;
+	public Text restartTekst;
+	public Text krajIgreTekst;
+
+	private int rezultat;
+	private bool krajIgre;
+	private bool restartujIgru;
+//	private new GUIText guiTekst;	
+
+
+	void Start () {
+		krajIgre = false;
+		restartujIgru = false;
+		restartTekst.text = "";
+		krajIgreTekst.text = "";
+//		guiTekst = GetComponent<GUIText> ();
+		rezultat = 0;
+		AzuriranjeRezultata ();
+		StartCoroutine (NaletiOpasnosti ());
+	}
+
+	void Update () {
+	
+		if (restartujIgru){
+			if (Input.GetKeyDown(KeyCode.R)){
+				Application.LoadLevel (Application.loadedLevel);
+			}
+		}
+	}
+
+	IEnumerator NaletiOpasnosti () {
+		yield return new WaitForSeconds (pocetakPauze);
+		while (true) {
+			for (int i = 0; i < brojNaleta; i++) {
+				GameObject opanostZaIgraca = opasnostiZaIgraca[Random.Range(0,opasnostiZaIgraca.Length)];
+				Vector3 pozicijaStvaranja = new Vector3 (Random.Range (-parametriAsteroida.x, parametriAsteroida.x), parametriAsteroida.y, parametriAsteroida.z);
+				Quaternion pozicijaRotiranja = Quaternion.identity;
+				Instantiate (opanostZaIgraca, pozicijaStvaranja, pozicijaRotiranja);
+				yield return new WaitForSeconds (pauzaUKreiranjuNaleta);
+			}
+			yield return new WaitForSeconds (pauzaUNaletima);
+
+			if (krajIgre){
+
+				restartTekst.text = "Pritisni 'R' da počneš ponovo";
+				restartujIgru = true;
+				break;
+			}
+		} 
+	}
+
+	public void DodavanjeRezultata (int noviRezultat){
+	
+		rezultat += noviRezultat;
+		AzuriranjeRezultata ();
+	}
+
+	void AzuriranjeRezultata (){
+	
+		rezultatTekst.text = "Rezultat: " + rezultat;
+	}
+
+	public void KrajIgre () {
+	
+		krajIgreTekst.text = "Kraj igre !";
+		krajIgre = true;
+	}
+}
